3b29c0d698e275c38602965fe4fb17a13accc871
akimiyaersoz <kimiyaersoz@icloud.com>
2025-04-04 05:56:38 +0000

    added homework files

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,29 +1,81 @@
 resource "aws_iam_user" "user1" {
-  name = "kaizen1"
+  name = "jenny"
 }
 
 resource "aws_iam_user" "user2" {
-  name = "kaizen2"
+  name = "rose"
 }
+
+resource "aws_iam_user" "user3" {
+  name = "lisa"
+}
+
+resource "aws_iam_user" "user4" {
+  name = "jisoo"
+}
+
+resource "aws_iam_user" "user5" {
+  name = "jihyo"
+}
+
+resource "aws_iam_user" "user6" {
+  name = "sana"
+}
+
+resource "aws_iam_user" "user7" {
+  name = "momo"
+}
+
+resource "aws_iam_user" "user8" {
+  name = "dahyun"
+}
+
 
 resource "aws_iam_group" "group1" {
-  name = "developers"
+  name = "blackpink"
 }
 
-resource "aws_iam_group_membership" "team" {
+resource "aws_iam_group" "group2" {
+  name = "twice"
+}
+
+
+resource "aws_iam_group_membership" "group1" {
   name = "tf-testing-group-membership"
 
   users = [
     aws_iam_user.user1.name,
-    aws_iam_user.user2.name
+    aws_iam_user.user2.name,
+    aws_iam_user.user3.name,
+    aws_iam_user.user4.name,
+    aws_iam_user.user9.name
   ]
     group = aws_iam_group.group1.name
   
 }
 
 
-resource "aws_iam_user" "user3" {
-  name = "hello"
+resource "aws_iam_group_membership" "group2" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.user5.name,
+    aws_iam_user.user6.name,
+    aws_iam_user.user7.name,
+    aws_iam_user.user8.name,
+    aws_iam_user.user10.name
+  ]
+    group = aws_iam_group.group2.name
+  
 }
+
+resource "aws_iam_user" "user9" {
+  name = "miyeon"
+}
+
+resource "aws_iam_user" "user10" {
+  name = "mina"
+}
+
 
  
