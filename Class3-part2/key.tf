4631fced97c8016ea3cb0bdda243261a61f1ae14
akimiyaersoz <kimiyaersoz@icloud.com>
2025-04-05 18:08:28 +0000

    New added class

---
+++ Class3-part2/key.tf
@@ -0,0 +1,21 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+tags ={
+  Name ="Hello"
+  Team = "DevOps"
+}
+
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "ayselinyo"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
